b533bf929526378360c5dba5dd28c608219eb115
rLaffuge <dalkspam@gmail.com>
2015-03-27 09:48:51 +0100

    Ajout partie projets + Ajout de la page erreur 404

--- public/Portfolio/assets/js/mainJQuery.js
+++ public/Portfolio/assets/js/mainJQuery.js
@@ -1,11 +1,23 @@
-$(function () {
+$(document).ready(function () {
+    //Animation de scrollspy
     $('li>a').on('click', function(e) {
         e.preventDefault();
         var hash = this.hash;
         $('html, body').animate({
-            scrollTop: $(this.hash).offset().top - 49
+            scrollTop: $(this.hash).offset().top
         }, 1000, function(){
             window.location.hash = hash;
         });
     });
+
+        $("[rel='tooltip']").tooltip();
+
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    );
 });
